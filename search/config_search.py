f4d53ef7f4eb9a6f0f72c07d141aef3c2b796916
Wuyang Chen <chenwydj@gmail.com>
2020-09-07 22:28:37 -0500

    update comments for configure files

--- search/config_search.py
+++ search/config_search.py
@@ -41,54 +41,54 @@ C.num_classes = 19
 C.background = -1
 C.image_mean = np.array([0.485, 0.456, 0.406])
 C.image_std = np.array([0.229, 0.224, 0.225])
-C.down_sampling = 2 # first down_sampling then crop ......
-C.image_height = 160 # this size is after down_sampling
-C.image_width = 160*2
+C.down_sampling = 2 # use downsampled images during search. In dataloader the image will first be down_sampled then cropped
+C.image_height = 160 # crop height after down_sampling in dataloader
+C.image_width = 160*2 # crop width after down_sampling in dataloader
 C.gt_down_sampling = 8 # model's default output size without final upsampling
-C.num_train_imgs = 2975
-C.num_eval_imgs = 500
+C.num_train_imgs = 2975 # number of training images
+C.num_eval_imgs = 500 # number of validation images
 
 """ Settings for network, this would be different for each kind of model"""
-C.bn_eps = 1e-5
-C.bn_momentum = 0.1
+C.bn_eps = 1e-5 # a value added to the BN denominator for numerical stability
+C.bn_momentum = 0.1 # value used for the running_mean and running_var computation
 
 """Train Config"""
-C.lr = 0.01
-C.momentum = 0.9
-C.weight_decay = 5e-4
-C.num_workers = 4
-C.train_scale_array = [0.75, 1, 1.25]
+C.lr = 0.01 # learning rate for updating supernet weight (NOT arch params)
+C.momentum = 0.9 # SGD momentum
+C.weight_decay = 5e-4 # SGD weight decay
+C.num_workers = 4 # workers for dataloader
+C.train_scale_array = [0.75, 1, 1.25] # scale factors for augmentation during training
 
 """Eval Config"""
-C.eval_stride_rate = 5 / 6
-C.eval_scale_array = [1, ]
-C.eval_flip = False
-C.eval_height = 1024
-C.eval_width = 2048
+C.eval_stride_rate = 5 / 6 # stride for crop based evaluation. Not used in this repo
+C.eval_scale_array = [1, ] # multi-scale evaluation
+C.eval_flip = False # flipping for evaluation
+C.eval_height = 1024 # real image height
+C.eval_width = 2048 # real image width
 
 
 """ Search Config """
-C.grad_clip = 5
-C.train_portion = 0.5
-C.arch_learning_rate = 3e-4
+C.grad_clip = 5 # grad clip for search
+C.train_portion = 0.5 # use how much % of training data for search
+C.arch_learning_rate = 3e-4 # learning rate for updating arch params
 C.arch_weight_decay = 0
-C.layers = 16
-C.branch = 2
+C.layers = 16 # layers (cells) for supernet
+C.branch = 2 # number of output branches
 
 C.pretrain = True
 # C.pretrain = "search-pretrain-256x512_F12.L16_batch3-20200101-012345"
 ########################################
-C.prun_modes = ['max', 'arch_ratio',]
-C.Fch = 12
-C.width_mult_list = [4./12, 6./12, 8./12, 10./12, 1.]
-C.stem_head_width = [(1, 1), (8./12, 8./12),]
-C.FPS_min = [0, 155.]
-C.FPS_max = [0, 175.]
+C.prun_modes = ['max', 'arch_ratio',] # channel pruning mode for [teacher, student], i.e. by default teacher will use max channel number, and student will sample the channel number based on arch_ratio
+C.Fch = 12 # base channel number
+C.width_mult_list = [4./12, 6./12, 8./12, 10./12, 1.] # selection choices for channel pruning
+C.stem_head_width = [(1, 1), (8./12, 8./12),] # width ratio (#channel / Fch) for [teacher, student]
+C.FPS_min = [0, 155.] # minimum FPS required for [teacher, student]
+C.FPS_max = [0, 175.] # maximum FPS allowed for [teacher, student]
 if C.pretrain == True:
     C.batch_size = 3
     C.niters_per_epoch = max(C.num_train_imgs // 2 // C.batch_size, 400)
     C.lr = 2e-2
-    C.latency_weight = [0, 0]
+    C.latency_weight = [0, 0] # weight of latency penalty loss
     C.image_height = 256 # this size is after down_sampling
     C.image_width = 256*2
     C.nepochs = 20
@@ -104,4 +104,4 @@ else:
 ########################################
 assert len(C.latency_weight) == len(C.stem_head_width) and len(C.stem_head_width) == len(C.FPS_min) and len(C.FPS_min) == len(C.FPS_max)
 
-C.unrolled = False
+C.unrolled = False # for DARTS v2
--- train/config_train.py
+++ train/config_train.py
@@ -44,48 +44,48 @@ C.background = -1
 C.image_mean = np.array([0.485, 0.456, 0.406])
 C.image_std = np.array([0.229, 0.224, 0.225])
 C.target_size = 1024
-C.down_sampling = 1 # first down_sampling then crop ......
-C.gt_down_sampling = 1
-C.num_train_imgs = 2975
-C.num_eval_imgs = 500
+C.down_sampling = 1 # use downsampled images during search. In dataloader the image will first be down_sampled then cropped
+C.gt_down_sampling = 1 # model's default output size without final upsampling
+C.num_train_imgs = 2975 # number of training images
+C.num_eval_imgs = 500 # number of validation images
 
 """ Settings for network, this would be different for each kind of model"""
-C.bn_eps = 1e-5
-C.bn_momentum = 0.1
+C.bn_eps = 1e-5 # a value added to the BN denominator for numerical stability
+C.bn_momentum = 0.1 # value used for the running_mean and running_var computation
 
 """Train Config"""
-C.lr = 0.01
-C.momentum = 0.9
-C.weight_decay = 5e-4
-C.nepochs = 600
-C.niters_per_epoch = 1000
-C.num_workers = 6
-C.train_scale_array = [0.75, 1, 1.25]
+C.lr = 0.01 # learning rate for updating supernet weight (NOT arch params)
+C.momentum = 0.9 # SGD momentum
+C.weight_decay = 5e-4 # SGD weight decay
+C.nepochs = 600 # how many epochs to train
+C.niters_per_epoch = 1000 # how many batches per epoch
+C.num_workers = 6 # workers for dataloader
+C.train_scale_array = [0.75, 1, 1.25] # scale factors for augmentation during training
 
 """Eval Config"""
-C.eval_stride_rate = 5 / 6
-C.eval_scale_array = [1, ]
-C.eval_flip = False
+C.eval_stride_rate = 5 / 6 # stride for crop based evaluation. Not used in this repo
+C.eval_scale_array = [1, ] # multi-scale evaluation
+C.eval_flip = False # flipping for evaluation
 C.eval_base_size = 1024
 C.eval_crop_size = 1024
-C.eval_height = 1024
-C.eval_width = 2048
+C.eval_height = 1024 # real image height
+C.eval_width = 2048 # real image width
 
 
-C.layers = 16
+C.layers = 16 # layers (cells) for network
 """ Train Config """
 C.mode = "student" # "teacher" or "student"
 if C.mode == "teacher":
     ##### train teacher model only ####################################
     C.arch_idx = [0] # 0 for teacher
-    C.branch = [2]
-    C.width_mult_list = [4./12, 6./12, 8./12, 10./12, 1.,]
-    C.stem_head_width = [(1, 1)]
+    C.branch = [2] # number of output branches
+    C.width_mult_list = [4./12, 6./12, 8./12, 10./12, 1.,] # selection choices for channel pruning
+    C.stem_head_width = [(1, 1)] # width ratio (#channel / Fch) for [teacher, student]
     C.load_path = "fasterseg" # path to the searched directory
     C.load_epoch = "last" # "last" or "int" (e.g. "30"): which epoch to load from the searched architecture
     C.batch_size = 12
-    C.Fch = 12
-    C.image_height = 512
+    C.Fch = 12 # base channel number
+    C.image_height = 512 # crop size for training
     C.image_width = 1024
     C.save = "%dx%d_teacher_batch%d"%(C.image_height, C.image_width, C.batch_size)
 elif C.mode == "student":
